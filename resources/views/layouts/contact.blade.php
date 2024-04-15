de3eec75deafc9b3152459089721f5cb2f3b18d7
abdelhafid <abde.elkhamssa@gmail.com>
2024-04-15 21:47:44 +0100

    Initial commit

---
+++ resources/views/layouts/contact.blade.php
@@ -0,0 +1,39 @@
+<style>
+    .containerd {
+        background-color: #ffcccc; /* Rose color */
+        padding: 50px 0; /* Optional: Add padding to the section */
+    }
+    .center {
+        text-align: center;
+    }
+</style>
+<section class="contact_section">
+    <div class="containerd px-0">
+        <div class="heading_container">
+            <h2 class="center">Contact Us</h2>
+        </div>
+    </div>
+    <div class="container container-bg">
+        <div class="row justify-content-center"><!-- Centering the content -->
+            <div class="col-md-6 col-lg-5"><!-- Adjusting column width -->
+                <form action="#">
+                    <div class="form-group text-center"><!-- Centering text inside the form group -->
+                        <input type="text" class="form-control" placeholder="Name" />
+                    </div>
+                    <div class="form-group text-center"><!-- Centering text inside the form group -->
+                        <input type="email" class="form-control" placeholder="Email" />
+                    </div>
+                    <div class="form-group text-center"><!-- Centering text inside the form group -->
+                        <input type="text" class="form-control" placeholder="Phone" />
+                    </div>
+                    <div class="form-group text-center"><!-- Centering text inside the form group -->
+                        <input type="text" class="form-control message-box" placeholder="Message" />
+                    </div>
+                    <div class="form-group d-flex justify-content-center"><!-- Centering the button -->
+                        <button class="btn btn-primary">SEND</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/layouts/css.blade.php
@@ -0,0 +1,21 @@
+<!-- slider stylesheet -->
+<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+
+<!-- bootstrap core css -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+<!-- Custom styles for this template -->
+<link href="css/style.css" rel="stylesheet" />
+<!-- responsive style -->
+<link href="css/responsive.css" rel="stylesheet" />
+<!-- font awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<style>
+    .fa-solid {
+        font-size: 2em; /* Adjust the size as needed */
+    }
+    body{
+        background-color: pink;
+    }
+</style>
